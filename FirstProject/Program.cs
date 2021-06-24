d528952d03e7e53e8b756ffbe9ca929d9974d2ed
VivekG11 <vivekvk2903@gmail.com>
2021-06-24 13:33:41 +0530

    first commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstvar = 11;
+            string secondvar = "vivek";
+            Console.WriteLine(firstvar + secondvar);
+            Console.WriteLine("MyFirstProject");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
